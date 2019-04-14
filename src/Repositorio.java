c120ec033985fd2dea1fe82a87fd34c543774bd1
Hugo Rebelo <HugoCamposRebelo@gmail.com>
2019-04-14 19:22:27 +0100

    Criação da camada de abstração no repositório

--- src/Repositorio.java
+++ src/Repositorio.java
@@ -2,19 +2,19 @@ import java.util.ArrayList;
 
 public interface Repositorio{
 
-    public abstract void adicionaUtilizador(Utilizador utilizador);
-    public abstract void adicionaTipoUtilizador(TipoUtilizador tipo);
-    public abstract void adicionaRequisicao(Requisicao requisicao);
-    public abstract void adicionaCopia(Copia copia);
-    public abstract void adicionaLivro(Livro livro);
-    public abstract void adicionaEmprestimo(Emprestimo emprestimo);
-    public abstract void adicionaNotificacao(Notificacao notificacao);
-    public abstract void adicionaDevolucao(Devolucao devolucao);
-    public abstract void adicionaCoima(Coima coima);
-    public abstract void adicionaEncomenda(Encomenda encomenda);
-    public abstract void adicionaEntradaNovoLivro(EntradaNovoLivro entradaNovoLivro);
-    public abstract void adicionaPropostaRequisicao(PropostaAquisicao proposta);
-    public abstract void adicionaRequisicaoCompra(RequisicaoCompra requisicaoCompra);
-    public abstract void adicionaRequisicaoCompraLivro(RequisicaoCompraLivro requisicaoCompraLivro);
-    public abstract Emprestimo devolveEmprestimoDaRequisicao(Requisicao r);
+    public void adicionaUtilizador(Utilizador utilizador);
+    public void adicionaTipoUtilizador(TipoUtilizador tipo);
+    public void adicionaRequisicao(Requisicao requisicao);
+    public void adicionaCopia(Copia copia);
+    public void adicionaLivro(Livro livro);
+    public void adicionaEmprestimo(Emprestimo emprestimo);
+    public void adicionaNotificacao(Notificacao notificacao);
+    public void adicionaDevolucao(Devolucao devolucao);
+    public void adicionaCoima(Coima coima);
+    public void adicionaEncomenda(Encomenda encomenda);
+    public void adicionaEntradaNovoLivro(EntradaNovoLivro entradaNovoLivro);
+    public void adicionaPropostaRequisicao(PropostaAquisicao proposta);
+    public void adicionaRequisicaoCompra(RequisicaoCompra requisicaoCompra);
+    public void adicionaRequisicaoCompraLivro(RequisicaoCompraLivro requisicaoCompraLivro);
+    public Emprestimo devolveEmprestimoDaRequisicao(Requisicao r);
 }
